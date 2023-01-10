7b6609265e001d7ec6eaec58164902bddc94ad9e
Setsudan <cryptiareta@gmail.com>
2023-01-10 11:10:39 +0100

    frontend: Login/Register UI added

--- frontend/src/routes/Auth/components/Register.tsx
+++ frontend/src/routes/Auth/components/Register.tsx
@@ -1,4 +1,13 @@
-import { Input, NumberInput, PasswordInput, Select } from "@mantine/core";
+import {
+  Input,
+  NumberInput,
+  PasswordInput,
+  Select,
+  Button,
+  createStyles,
+  Title,
+  Divider,
+} from "@mantine/core";
 import { useDispatch } from "react-redux";
 import { useState } from "react";
 import { register } from "../../../func/auth.func";
@@ -19,38 +28,47 @@ export default function Register() {
     );
   };
 
+  const { classes } = useStyle();
+
   return (
-    <>
+    <div className={classes.registerFormWrapper}>
       <form
+        className={classes.registerForm}
         onSubmit={(e) => {
           e.preventDefault();
         }}
       >
+        <Title order={1}>Register</Title>
         <Input
+          className={classes.formInput}
           placeholder="Email"
           value={email}
           onChange={(e) => setEmail(e.currentTarget.value)}
           required
         />
         <Input
+          className={classes.formInput}
           placeholder="First Name"
           value={firtName}
           onChange={(e) => setFirstName(e.currentTarget.value)}
           required
         />
         <Input
+          className={classes.formInput}
           placeholder="Last Name"
           value={lastName}
           onChange={(e) => setLastName(e.currentTarget.value)}
           required
         />
         <PasswordInput
+          className={classes.formInput}
           placeholder="Password"
           value={password}
           onChange={(e) => setPassword(e.currentTarget.value)}
           required
         />
         <Select
+          className={classes.formInput}
           value={gender}
           data={[
             { label: "Male", value: "Male" },
@@ -64,6 +82,7 @@ export default function Register() {
           required
         />
         <NumberInput
+          className={classes.formInput}
           placeholder="Age"
           value={age}
           onChange={(e) => {
@@ -73,14 +92,53 @@ export default function Register() {
           }}
           required
         />
-        <button
+        <Button
           onClick={() => {
             handleSubmit();
           }}
         >
           Register
-        </button>
+        </Button>
+        <Divider my="sm" className={classes.divider} />
+        <Button
+          onClick={() => {
+            window.location.href = "/auth/login";
+          }}
+          variant="subtle"
+        >
+          Login
+        </Button>
       </form>
-    </>
+    </div>
   );
 }
+
+const useStyle = createStyles((theme) => ({
+  registerForm: {
+    display: "flex",
+    minHeight: "50vh",
+    padding: "2vh",
+    width: "30vw",
+    justifyContent: "space-around",
+    alignItems: "center",
+    flexDirection: "column",
+    border: "1px solid #ccc",
+    borderRadius: "18px",
+  },
+  registerFormWrapper: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100vh",
+  },
+  formInput: {
+    width: "50%",
+    margin: "1vh 0",
+  },
+  divider: {
+    width: "100%",
+    height: "1px",
+    backgroundColor: "#ccc",
+  },
+}));
--- frontend/src/routes/Auth/index.tsx
+++ frontend/src/routes/Auth/index.tsx
@@ -7,7 +7,25 @@ import { useParams } from "react-router-dom";
 import Login from "./components/Login";
 import Register from "./components/Register";
 
+import { Center } from "@mantine/core";
+
+import { createStyles } from "@mantine/core";
+
 export default function Auth() {
+  const { classes } = useStyle();
   const { authtype } = useParams();
-  return <div>{authtype === "login" ? <Login /> : <Register />}</div>;
+  return (
+    <main className={classes.main}>
+      <Center>{authtype === "login" ? <Login /> : <Register />}</Center>
+    </main>
+  );
 }
+
+const useStyle = createStyles((theme) => ({
+  main: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100vh",
+  },
+}));
