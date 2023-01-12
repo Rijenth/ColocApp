5e1cc41b5d5cfa656532ca4b7c2c4471e38ab839
Setsu | A.K.A EthLny <60669080+Setsudan@users.noreply.github.com>
2023-01-12 15:21:01 +0100

    frontend remade without redux (#17)

--- frontend/src/routes/NotFound/index.tsx
+++ frontend/src/routes/NotFound/index.tsx
@@ -1,13 +1,9 @@
-// using mantine and redux we will create a new component
-
-import { useParams } from "react-router-dom";
-
-// inner components
-import {Error404} from "./components/Error404";
+import { Error404 } from "./components/Error404";
 
 export default function NotFound() {
-    const { parms } = useParams();
-    return <div>
-        <Error404 />
-    </div>;
+  return (
+    <div>
+      <Error404 />
+    </div>
+  );
 }
