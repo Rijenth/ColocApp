63340a810542f0d23ab8fc80577b7d35a059b010
LFLeveneur <73178352+LFLeveneur@users.noreply.github.com>
2023-01-13 09:28:33 +0100

    ➖ Remove axios

--- frontend/src/func/dashboard.func.tsx
+++ frontend/src/func/dashboard.func.tsx
@@ -1,10 +1,6 @@
-import axios from 'axios';
-
-import { ExpensePayload } from '../interfaces/data.interface';
-
 const API_URL = 'http://localhost:8080/api/';
 
-// Auth Header for axios
+// Auth Header for fetch
 const authHeader = () => {
     const user = JSON.parse(sessionStorage.getItem('coloc-user') || '{}');
     if (user && user.token) {
@@ -15,18 +11,25 @@ const authHeader = () => {
 }
 
 export function createExpenses(payload: { createdAt: string; amount: string; desccription: string; paidFord: string; colocataireId: string; updateAt: string; colocationId: string }) {
-  return axios.post(`${API_URL}/expense/create`, payload, { headers: authHeader() });
+    return fetch(`${API_URL}/expense/create`, {
+        method: 'POST',
+        body: JSON.stringify(payload),
+        headers: {...authHeader(), 'Content-Type': 'application/json'}
+    });
 }
 
 export function getExpenses() {
-  return axios.get(`${API_URL}/expense`, { headers: authHeader() });
+    return fetch(`${API_URL}/expense`, {headers: authHeader()});
 }
 
 export function getExpensesById(id: string) {
-    return axios.get(`${API_URL}/expense/${id}`, { headers: authHeader() });
+    return fetch(`${API_URL}/expense/${id}`, {headers: authHeader()});
 }
 
 export function updateExpenses(id: string, payload: ExpensePayload) {
-    return axios.put(`${API_URL}/expense/${id}`, payload, { headers: authHeader() });
+    return fetch(`${API_URL}/expense/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify(payload),
+        headers: {...authHeader(), 'Content-Type': 'application/json'}
+    });
 }
-
